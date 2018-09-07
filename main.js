ff2bff6761a51f973879161333f59b2848590723
Christopher Quadflieg <chrissi92@hotmail.de>
2018-09-07 21:00:45 +0200

    init vue in electron

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
 const { app, BrowserWindow } = require('electron');
 
-let mainWindow;
-
-function createWindow() {
-	mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
-	mainWindow.loadFile('index.html');
-
-	mainWindow.on('closed', function() {
-		mainWindow = null;
-	});
+let url;
+if (process.env.NODE_ENV === 'DEV') {
+	url = 'http://localhost:8081/';
+} else {
+	url = `file://${process.cwd()}/dist/index.html`;
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function() {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on('ready', () => {
+	let window = new BrowserWindow({
+		width: 800,
+		height: 600,
+		autoHideMenuBar: true,
+		skipTaskbar: true
+	});
+	window.loadURL(url);
 });
 
-app.on('activate', function() {
-	if (mainWindow === null) {
-		createWindow();
-	}
-});
+// const { app, BrowserWindow } = require('electron');
+
+// let mainWindow;
+
+// function createWindow() {
+// 	mainWindow = new BrowserWindow({
+// 		width: 800,
+// 		height: 600,
+// 		autoHideMenuBar: true,
+// 		skipTaskbar: true
+// 	});
+
+// 	mainWindow.loadFile('index.html');
+
+// 	mainWindow.on('closed', function() {
+// 		mainWindow = null;
+// 	});
+// }
+
+// app.on('ready', createWindow);
+
+// app.on('window-all-closed', function() {
+// 	if (process.platform !== 'darwin') {
+// 		app.quit();
+// 	}
+// });
+
+// app.on('activate', function() {
+// 	if (mainWindow === null) {
+// 		createWindow();
+// 	}
+// });
