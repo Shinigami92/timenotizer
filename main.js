fc3912ac9b53a81ba3cff9eef90bd445630cef90
Christopher Quadflieg <chrissi92@hotmail.de>
2018-09-08 16:20:28 +0200

    close via tray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 
 let url;
 if (process.env.NODE_ENV === 'DEV') {
@@ -7,46 +7,48 @@ if (process.env.NODE_ENV === 'DEV') {
 	url = `file://${process.cwd()}/dist/index.html`;
 }
 
-app.on('ready', () => {
-	let window = new BrowserWindow({
+let mainWindow;
+let mainWindowPresent = false;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({
 		// width: 800,
 		// height: 600,
 		useContentSize: true,
 		autoHideMenuBar: true,
-		skipTaskbar: true
+		skipTaskbar: false
 	});
-	window.loadURL(url);
+	mainWindow.loadURL(url);
+	mainWindowPresent = true;
+}
+
+app.on('ready', () => {
+	let tray = new Tray('src/assets/logo.png');
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Quit TimeNotizer',
+			click() {
+				// Program can be closed via tray -> quit
+				app.quit();
+			}
+		}
+	]);
+	tray.setToolTip('TimeNotizer');
+	tray.setContextMenu(contextMenu);
+	tray.on('click', (event, bounds, position) => {
+		console.log({ event, bounds, position });
+		console.log({ mainWindow });
+		if (mainWindowPresent === false) {
+			createWindow();
+		}
+	});
+
+	createWindow();
 });
 
-// const { app, BrowserWindow } = require('electron');
-
-// let mainWindow;
-
-// function createWindow() {
-// 	mainWindow = new BrowserWindow({
-// 		width: 800,
-// 		height: 600,
-// 		autoHideMenuBar: true,
-// 		skipTaskbar: true
-// 	});
-
-// 	mainWindow.loadFile('index.html');
-
-// 	mainWindow.on('closed', function() {
-// 		mainWindow = null;
-// 	});
-// }
-
-// app.on('ready', createWindow);
-
-// app.on('window-all-closed', function() {
-// 	if (process.platform !== 'darwin') {
-// 		app.quit();
-// 	}
-// });
-
-// app.on('activate', function() {
-// 	if (mainWindow === null) {
-// 		createWindow();
-// 	}
-// });
+app.on('window-all-closed', (event) => {
+	console.log({ event });
+	// Program should not close on exit
+	event.preventDefault();
+	mainWindowPresent = false;
+});
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import './plugins/vuetify';
+import Vue, { CreateElement, VNode } from 'vue';
 import App from './App.vue';
+import './plugins/vuetify';
 import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
 	store,
-	render: (h) => h(App),
+	render: (h: CreateElement): VNode => h(App)
 }).$mount('#app');
